6861b6b8fe59ddead46b3b62a864983d62f9a2f1
Luke Terrell <luke.terrell43@gmail.com>
2021-03-19 19:33:28 -0500

    added function to listen for button click and filter data based on date

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ function buildTable(data) {
     // Next loop through each object in the data and append a row and ell for each value
     data.forEach ((dataRow) => {
         //  Append a row to the table body
-        let row = tbody.append("tr"):
+        let row = tbody.append("tr");
 
     // Loop through each field in the dataRow and add each value as a table cell (td)
     Object.values(dataRow).forEach((val) => {
@@ -20,3 +20,25 @@ function buildTable(data) {
     );
     });
 }
+
+// add button click to filter the data
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // Check to see if date was entered and filter the date with it
+    if (date) {
+        // Apply filter to table data and keep data where date matches filter
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild table using filtered data
+    // @Note: if no date entered, filteredData == tableData
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData)
